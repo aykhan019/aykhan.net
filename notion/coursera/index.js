6d75ef004bc5b287cc67064154559a61088c0d65
Aykhan Ahmadzada <ayxanx17@gmail.com>
2024-09-02 02:15:29 +0400

    Fixed Relative File Paths

--- notion/coursera/index.js
+++ notion/coursera/index.js
@@ -8,14 +8,14 @@ const ibm_ai_engineering = new PageLinkItem(
     "https://aykhan.net/notion/coursera/ibm-ai-engineering"
 );
 
-const ibm_data_science = new PageLinkItem(
+const ibm_data_analysis = new PageLinkItem(
     2, 
     "IBM Data Science", 
     "Notes from IBM Data Science courses on Coursera.", 
     "https://media.aykhan.net/thumbnails/notion/coursera/ibm/ibm-v1.png", 
-    "https://aykhan.net/notion/coursera/ibm-data-science"
+    "https://aykhan.net/notion/coursera/ibm-data-analysis"
 );
 
-const specializations = [ibm_ai_engineering, ibm_data_science];
+const specializations = [ibm_ai_engineering, ibm_data_analysis];
 
 addItemsToContainer(container, specializations);
