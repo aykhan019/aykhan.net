b596e155e2d16aa9dd167497d51267d13cd57889
Aykhan Ahmadzada <ayxanx17@gmail.com>
2024-09-04 12:13:22 +0400

    Fixed IBM Data Analysis Notes Redirect

--- notion/coursera/ibm-data-analysis/index.js
+++ notion/coursera/ibm-data-analysis/index.js
@@ -4,8 +4,8 @@ const data_science_course = new PageLinkItem(
     1, 
     "Data Analysis with Python", 
     "Notes from the Data Analysis with Python course on Coursera.", 
-    "https://media.aykhan.net/thumbnails/notion/coursera/ibm/data-analysis-with-python.png", 
-    "https://aykhan.net/notion/coursera/ibm-data-science/data-analysis-with-python"
+    "https://media.aykhan.net/thumbnails/notion/coursera/ibm/ibm-data-analysis/data-analysis-with-python.png", 
+    "https://aykhan.net/notion/coursera/ibm-data-analysis/data-analysis-with-python"
 );
 
 addItemsToContainer(container, [data_science_course]);
